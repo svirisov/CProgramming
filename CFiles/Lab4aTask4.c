4bb3d2e77935df212642251bad553f1f7bc3e5f9
fenex <svirisov@gmail.com>
2020-02-21 12:58:45 +0000

    Correct error in variable deffinition

--- CFiles/Lab4aTask4.c
+++ CFiles/Lab4aTask4.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 int main (void){
-  int i, fact;
+  int i, fact = 1;
 
   printf("Please enter a number: ");
   scanf("%d", &i);
-  
+
   for(int j = 1; j <= i; j++){
-    fact = fact*j;
+    fact *= j;
   }
 
-  printf("%d! equals %d\n",i,fact);
+  printf("%d equals %d\n",i,fact);
 
   return 0;
 }
